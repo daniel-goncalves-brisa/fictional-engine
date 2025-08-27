3572bf8fe9726c61aa617eb6049be557d701b75a
daniel-goncalves-brisa <daniel.goncalves@grupobrisanet.com.br>
2025-08-27 09:30:25 -0300

    feat: add ":" on startup message

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 }
 
 module.exports = app;
