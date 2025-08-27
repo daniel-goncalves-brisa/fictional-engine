7508eb2b06dcb8e8c92315d67737ba99b1da5055
daniel-goncalves-brisa <daniel.goncalves@grupobrisanet.com.br>
2025-08-27 09:32:09 -0300

    fix: remove spoiler comments

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const authRoutes = require('./routes/auth');
 
 const app = express();
 app.use(bodyParser.json());
-// app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== 'test') {
